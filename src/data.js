1f22dac3172d73795c0248a815d4f6641727084a
mdowaisnazi <71321540+mdowaisnazi@users.noreply.github.com>
2022-10-21 12:22:41 -0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/X2vbS4G/Screenshot-2022-10-04-14-36-50-85.jpg",
+      link: "http://nimahandloom.com",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/GFKH2b2/H.jpg",
+      link: "http://healtmedin.com",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/X2vbS4G/Screenshot-2022-10-04-14-36-50-85.jpg",
+      link: "http://nimahandloom.com",
+    },
+    {
+      id: 4,
+      img: "https://i.ibb.co/GFKH2b2/H.jpg",
+      link: "http://healtmedin.com",
+    },
+    {
+      id: 5,
+      img: "https://i.ibb.co/X2vbS4G/Screenshot-2022-10-04-14-36-50-85.jpg",
+      link: "http://nimahandloom.com",
+    },
+    {
+      id: 6,
+      img: "https://i.ibb.co/GFKH2b2/H.jpg",
+      link: "http://healtmedin.com",
+    },
+  ];
